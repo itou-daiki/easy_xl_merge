652b0d0c600e7a8b81862c05a0148ae865ec60b6
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2023-12-20 12:39:46 +0000

    修正

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,37 +4,39 @@ import openpyxl
 
 st.set_page_config(page_title="easy Excel Join")
 
-# App title and creator
 st.title("easy Excel Join")
 st.subheader("データフレーム結合Webアプリケーション")
 st.caption("Created by Dit-Lab.(Daiki Ito)")
 
 # ファイルアップロードセクション
 file1 = st.file_uploader("１つ目のExcelファイルをアップロード", type=['xlsx'])
+# Excelファイルをデータフレームに読み込む
+df1 = pd.read_excel(file1)
+# プレビュー
+st.subheader("＜１つ目のExcelファイル＞")
+st.dataframe(df1)
+
 file2 = st.file_uploader("２つ目のExcelファイルをアップロード", type=['xlsx'])
+# Excelファイルをデータフレームに読み込む
+df2 = pd.read_excel(file2)
+# プレビュー
+st.subheader("＜２つ目のExcelファイル＞")
+st.dataframe(df2)
 
 if file1 and file2:
-    # Excelファイルをデータフレームに読み込む
-    df1 = pd.read_excel(file1)
-    # プレビュー
-    st.subheader("＜１つ目のExcelファイル＞")
-    st.dataframe(df1)
-
-    df2 = pd.read_excel(file2)
-    # プレビュー
-    st.subheader("＜２つ目のExcelファイル＞")
-    st.dataframe(df2)
-
-    # 同じ名前のカラムがある場合の警告
-    common_columns = set(df1.columns).intersection(df2.columns)
+    # 共通するカラム名を取得
+    common_columns = list(set(df1.columns).intersection(df2.columns))
     if common_columns:
-        st.warning(f'両方のファイルに以下の同じ名前のカラムが存在します: {common_columns}. 結合時のエラーを避けるため、異なるカラム名を使用してください。')
+        # キーとなるカラムをユーザーに選択させる
+        key_column = st.selectbox('結合に使用するキーとなるカラムを選択してください:', common_columns)
 
-    # データフレームの結合
-    joined_df = df1.join(df2, lsuffix='_left', rsuffix='_right')
+        # データフレームの結合
+        joined_df = pd.merge(df1, df2, on=key_column)
 
-    # 結合したデータを表示
-    st.write("結合されたデータ:", joined_df)
+        # 結合したデータを表示
+        st.write("結合されたデータ:", joined_df)
+    else:
+        st.warning('共通するカラムが存在しません。結合するには共通のカラムが必要です。')
 
 # Copyright
 st.markdown('© 2022-2023 Dit-Lab.(Daiki Ito). All Rights Reserved.')
